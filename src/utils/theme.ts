ca63d17c4349c67e6614bde8b887a0c044fc9af7
Roshan K Gujarathi <roshankumargujarathi@gmail.com>
2023-12-28 16:49:13 +0530

    Text component

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,21 +1,143 @@
 import '@emotion/react'
 
 export const lightTheme = {
-    colors: {
-        primary: 'hotpink'
+  fontFace: {
+    fontFamily: 'Roboto',
+    src: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
+  },
+  typography: {
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: '1.75rem',
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: '1.125rem',
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: '1rem',
+      fontWeight: 500,
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+    },
+    body1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+    },
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+    },
+    button: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      textTransform: 'none',
+    },
+    caption: {
+      fontSize: '0.75rem',
+      fontWeight: 400,
+    },
+    overline: {
+      fontSize: '0.75rem',
+      fontWeight: 500,
+      textTransform: 'none',
     }
+  },
+  colors: {
+    primary: 'hotpink',
+    text: '#000',
+  }
 }
 
 export const darkTheme = {
-    colors: {
-        primary: 'yellow'
-    }
+  colors: {
+    primary: 'yellow'
+  }
 }
 
 declare module '@emotion/react' {
-    export interface Theme {
-      colors: {
-        primary: string
+  export interface Theme {
+    fontFace: {
+      fontFamily: string
+      src: string
+    }
+    colors: {
+      primary: string
+      text: string
+    }
+    typography: {
+      h1: {
+        fontSize: string
+        fontWeight: number
+      }
+      h2: {
+        fontSize: string
+        fontWeight: number
+      }
+      h3: {
+        fontSize: string
+        fontWeight: number
+      }
+      h4: {
+        fontSize: string
+        fontWeight: number
+      }
+      h5: {
+        fontSize: string
+        fontWeight: number
+      }
+      h6: {
+        fontSize: string
+        fontWeight: number
+      }
+      subtitle1: {
+        fontSize: string
+        fontWeight: number
+      }
+      subtitle2: {
+        fontSize: string
+        fontWeight: number
+      }
+      body1: {
+        fontSize: string
+        fontWeight: number
+      }
+      body2: {
+        fontSize: string
+        fontWeight: number
+      }
+      button: {
+        fontSize: string
+        fontWeight: number
+        textTransform: string
+      }
+      caption: {
+        fontSize: string
+        fontWeight: number
+      }
+      overline: {
+        fontSize: string
+        fontWeight: number
+        textTransform: string
       }
     }
   }
+}
