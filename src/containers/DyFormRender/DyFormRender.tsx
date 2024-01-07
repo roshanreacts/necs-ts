e162d591eb30bf14e40513064507611962307cd3
Adarsh M <Adarsh.M@resmed.com.au>
2024-01-07 22:24:09 +0530

    enum options are visible only when type is enum.

--- src/containers/DyFormRender/DyFormRender.tsx
+++ src/containers/DyFormRender/DyFormRender.tsx
@@ -1,30 +1,41 @@
-"use client"
-import React from 'react'
+"use client";
+import { startCase } from "lodash";
+import React from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
 export default function DyFormRender({ fields }: { fields: any }) {
     const [dynamicFields, setDynamicFields] = React.useState<any[]>([]);
 
     React.useEffect(() => {
-        setDynamicFields(Object.entries(fields).map(([fieldSlug, fieldValue]: [string, any]) => ({ [fieldSlug]: fieldValue })));
+        setDynamicFields(
+            Object.entries(fields).map(([fieldSlug, fieldValue]: [string, any]) => ({
+                [fieldSlug]: fieldValue,
+            }))
+        );
     }, [fields]);
 
     const addNewField = () => {
         const newFieldSlug = `newField${dynamicFields.length + 1}`;
-        const newFieldValue = { label: '', type: 'text', isEditable: false, required: false }; // Set default values
+        const newFieldValue = {
+            label: "",
+            type: "text",
+            isEditable: false,
+            required: false,
+        }; // Set default values
         setDynamicFields([...dynamicFields, { [newFieldSlug]: newFieldValue }]);
     };
 
     return (
         <>
-            {dynamicFields.length && dynamicFields.map((dynamicField) => {
-                const [fieldSlug, fieldValue] = Object.entries(dynamicField)[0];
-                return (
-                    <div key={fieldSlug} style={{ padding: '10px' }}>
-                        <DynamicForm fieldSlug={fieldSlug} fieldValue={fieldValue} />
-                    </div>
-                );
-            })}
+            {dynamicFields.length &&
+                dynamicFields.map((dynamicField) => {
+                    const [fieldSlug, fieldValue] = Object.entries(dynamicField)[0];
+                    return (
+                        <div key={fieldSlug} style={{ padding: "10px" }}>
+                            <DynamicForm fieldSlug={fieldSlug} fieldValue={fieldValue} />
+                        </div>
+                    );
+                })}
             <button onClick={addNewField}>Add New Field</button>
         </>
     );
@@ -40,69 +51,127 @@ function DynamicForm({ fieldSlug, fieldValue }: DynamicFormProps) {
         register,
         control,
         handleSubmit,
+        setValue,
+        watch,
         formState: { errors },
     } = useForm<any>();
+    const [formKey, setFormKey] = React.useState(0);
+
+    const selectedType = watch("type");
+
+    const setFieldValue = (field: string, value: any) => {
+        setValue(field, value);
+        setFormKey((prevKey) => prevKey + 1);
+    };
 
     const onSubmit: SubmitHandler<any> = (data) => {
         console.log(`Form for ${fieldSlug} submitted with data:`, data);
     };
 
+    const renderField = (
+        field: any,
+        props: any,
+        control: any,
+        setFieldValue: Function
+    ) => {
+        const options = [
+            { value: "string", label: "String" },
+            { value: "enum", label: "ENUM" },
+            { value: "boolean", label: "Boolean" },
+            { value: "relationship", label: "Relationship" },
+            { value: "bcrypt", label: "Bcrypt" },
+        ];
+
+        switch (field) {
+            case "label":
+                return (
+                    <>
+                        {startCase(field)}
+                        <input {...props} onChange={(e) => setFieldValue(field, e.target.value)} />
+                        ;
+                    </>
+                );
+            case "default":
+                if (props.name) {
+                    // add a condition to check the default value field is for ENUM, string, checkbox.
+                    return (
+                        <>
+                            {startCase(field)}
+                            <input {...props} onChange={(e) => setFieldValue(field, e.target.value)} />
+                        </>
+                    );
+                }
+            case "enum":
+                return (
+                    <>
+                        {(selectedType === undefined || selectedType === 'enum') && (
+                            <>
+                                <p>{startCase(field)}</p>
+                                <textarea {...props} onChange={(e) => setFieldValue(field, e.target.value)} />
+                            </>
+                        )}
+                    </>
+                );
+            case "type":
+                return (
+                    <>
+                        {startCase(field)}
+                        <select {...props} multiple={false} onChange={(e) => setFieldValue(field, e.target.value)}>
+                            {options.map((item: any) => (
+                                <option key={item.value} value={item.value}>
+                                    {item.label}
+                                </option>
+                            ))}
+                        </select>
+                    </>
+                );
+            case "isEditable":
+            case "required":
+                return (
+                    <>
+                        {startCase(field)}
+                        <input
+                            type="checkbox"
+                            {...props}
+                            onChange={(e) => setFieldValue(field, e.target.checked)}
+                        />
+                    </>
+                );
+            default:
+                return (
+                    <>
+                        {startCase(field)}
+                        <input type="checkbox" {...props} onChange={(e) => setFieldValue(field, e.target.checked)} />
+                    </>
+                );
+        }
+    };
+
     return (
-        <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+        <form key={formKey} onSubmit={handleSubmit((data) => onSubmit(data))}>
             {Object.entries(fieldValue).map(([key, value]: [string, any]) => {
                 return (
-                    <div key={key + value} style={{ display: 'flex' }}>
-                        {key}
-                        {renderField(key, { ...register(key), defaultValue: value ?? undefined }, control)}
+                    <div
+                        key={fieldSlug + key}
+                        style={{ display: "flex", gap: "6px", margin: "6px 0" }}
+                    >
+                        {renderField(
+                            key,
+                            {
+                                ...register(key),
+                                ...(Array.isArray(value) && key === "enum"
+                                    ? { defaultValue: value }
+                                    : Array.isArray(value) && key != "enum"
+                                        ? {}
+                                        : { defaultValue: value }),
+                            },
+                            control,
+                            setFieldValue
+                        )}
                     </div>
                 );
             })}
-            <input type="submit" value={`Submit ${fieldSlug}`} />
+            <input type="submit" value="Save" />
         </form>
     );
 }
-
-const renderField = (field: any, props: any, control: any) => {
-    const options = [
-        { value: 'string', label: 'String' },
-        { value: 'enum', label: 'ENUM' },
-        { value: 'boolean', label: 'Boolean' },
-        { value: 'relationship', label: 'Relationship' },
-        { value: 'bcrypt', label: 'Bcrypt' },
-    ];
-
-    switch (field) {
-        case 'label':
-            return <input {...props} />;
-        case 'enum':
-            if (props.name === "enum" && Array.isArray(props.defaultValue)) {
-                return (
-                    <select {...props}>
-                        {props.defaultValue.map((enumValue: string) => (
-                            <option key={enumValue} value={enumValue}>
-                                {enumValue}
-                            </option>
-                        ))}
-                    </select>
-                );
-            }
-            return <p>Error: Enum values not specified</p>;
-        case 'type':
-            return (
-                <>
-                    <select {...props} multiple={false}>
-                        {options.map((item: any) => (
-                            <option key={item.value} value={item.value}>
-                                {item.label}
-                            </option>
-                        ))}
-                    </select>
-                </>
-            );
-        case 'isEditable':
-        case 'required':
-            return <input type="checkbox" {...props} />;
-        default:
-            return null;
-    }
-};
