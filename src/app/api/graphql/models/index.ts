f23bf006aba93cd547415a91e2b5c513c38c8dc5
yashu2304 <18kq1a05a8@pace.ac.in>
2024-02-13 18:18:15 +0530

    mercury update

--- src/app/api/graphql/models/index.ts
+++ src/app/api/graphql/models/index.ts
@@ -2,8 +2,8 @@ import mercury from "@mercury-js/core";
 import platform from "@mercury-js/core/packages/platform"
 import redisCache from "@mercury-js/core/packages/redisCache";
 
-
-mercury.package([redisCache(),platform()]);
+// @ts-ignore
+mercury.package([redisCache({ url: process.env.KV_URL }),platform()]);
 
 export { User } from "./User.model";
 export { Account } from "./Account.model";
